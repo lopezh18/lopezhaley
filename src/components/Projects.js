71aa22b5f543f5fd5e25e438a048747b8ff35163
Haley <haleylopez95@gmail.com>
2019-07-10 16:56:10 -0700

    added jobly

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,16 @@ export default class Projects extends Component {
   render() {
     return (
       <div className='projects-container'>
+        <div className='projects'>
+          <div className='project-card'>
+            <div className='jobly'>
+            </div>
+            <div className='project-content'>
+              <h5>Jobly</h5>
+              <p> React • Node/Express • PostgreSQL </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
